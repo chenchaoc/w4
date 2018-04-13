9f5ed389360020021b567132322328ea03245b65
chenchao <chenchao3.sh@superjia.com>
2018-04-13 09:44:27 +0800

    fix

--- build/config/deployConfig.js
+++ build/config/deployConfig.js
@@ -3,14 +3,15 @@
 * @Date: 2018-04-12 18:46:45
 * @Email: chenchao3@sh.superjia.com
 * @Last Modified by: chenchao
-* @Last Modified time: 2018-04-12 18:49:50
+* @Last Modified time: 2018-04-13 09:44:16
 */
 export default {
     catalog: 'htdocs',
     project: 'w4',
     zipFilePath: `${process.cwd()}/uploadZip/w4.zip`,
     ftp: {
-        host: '60.205.34.245', //ip
-        user: 'bxu2442440084', //用户名
+        host: '', //ip
+        user: '', //用户名
+        password: '' //密码
     }
 }
